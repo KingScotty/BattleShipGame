1a925c7eedf102730957a1f08f4b0b9abdf26bfa
Scott Browning <scottybrowning12345@gmail.com>
2025-04-28 17:09:20 -0400

    built GameController.java this fixes remaining errors in MissileTracker and BattleSHipGame.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class GameController {
+    private GameBoard gameBoard;
+    private MissileTracker missileTracker;
+    private BattleShipGame game;
+
+public GameController(GameBoard gameBoard, MissileTracker missileTracker, BattleShipGame game) {
+    this.gameBoard = gameBoard;
+    this.missileTracker = missileTracker;
+    this.game = game;
 }
+    public void playAgain() {
+        int choice = JOptionPane.showConfirmDialog(game, "Do you want to play again?", "Play Again", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.YES_OPTION) {
+            game.resetGame();
+        }
+    }
+        public void quitGame () {
+            int choice = JOptionPane.showConfirmDialog(game, "Are you sure you want to quit?", "Quit Game", JOptionPane.YES_NO_OPTION);
+            if (choice == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        }
+    }
