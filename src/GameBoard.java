ab16c4ffea088c78d9eaa18a00463749063fc02c
Scott Browning <scottybrowning12345@gmail.com>
2025-04-28 15:01:50 -0400

    Need to wire it up to MissileTracker

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,41 @@
-public class GameBoard {
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+
+public class GameBoard extends JPanel {
+    private BoardCell[][] board;
+    private int rows;
+    private int cols;
+    private MissileTracker missileTracker;
+
+    public GameBoard(int rows, int cols, MissileTracker missileTracker) {
+        this.rows = rows;
+        this.cols = cols;
+        this.missileTracker = missileTracker;
+        board = new BoardCell[rows][cols];
+        setLayout(new GridLayout(rows, cols));
+        initializeBoard();
+    }
+
+    private void initializeBoard(){
+        for (int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                BoardCell cell = new BoardCell(row, col);
+                cells[row][col] = cell;
+                add(cell);
+            }
+        }
+    }
+//reset board
+    public void resetBoard() {
+        removeAll();
+        initializeBoard();
+        revalidate();
+        repaint();
+
+    }
+
+    public BoardCell getCell(int row, int col){
+        return cells[row][col];
+    }
 }
