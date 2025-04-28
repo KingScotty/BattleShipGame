e2d8f1bb743af8b315295d68c7654365a6efc3bc
Scott Browning <scottybrowning12345@gmail.com>
2025-04-28 14:36:47 -0400

    Created all the classes

--- src/BattleShipGame.java
+++ src/BattleShipGame.java
@@ -1,2 +1,13 @@
-public class BattleShipGame {
+import javax.swing.*;
+import java.awt.*;
+
+public class BattleShipGame extends JFrame {
+    private GameBoard gameBoard;
+    private StatusPanel statusPanel;
+    private GameController gameController;
+    private MissileTracker missileTracker;
+    private ShipPlacer shipPlacer;
+
+
+
 }
