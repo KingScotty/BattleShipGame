37734da1546cc0c85a1a9abc7f9d04713c68789e
Scott Browning <scottybrowning12345@gmail.com>
2025-04-28 14:49:12 -0400

    built constructors. now need to wire them up to the other classes

--- src/BattleShipGame.java
+++ src/BattleShipGame.java
@@ -2,12 +2,48 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BattleShipGame extends JFrame {
+    //other classes
     private GameBoard gameBoard;
     private StatusPanel statusPanel;
     private GameController gameController;
     private MissileTracker missileTracker;
     private ShipPlacer shipPlacer;
+    //game stuff
+    public BattleShipGame() {
+        setTitle("BattleShip Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLayout(new BorderLayout());
 
+        missileTracker = new MissileTracker(this);
+        statusPanel = new StatusPanel(missileTracker);
+        gameBoard = new GameBoard(10, 10, missileTracker);
+        shipPlacer = new ShipPlacer(gameBoard);
+        gameController = new GameController(gameBoard, missileTracker, this);
 
-
+        // Add components to the frame
+        add(statusPanel, BorderLayout.NORTH);
+        add(gameBoard, BorderLayout.CENTER);
+        add(createButtonPanel(), BorderLayout.SOUTH);
+        shipPlacer.placeShips();
+        setVisible(true);
+    }
+//reset
+public void resetGame() {
+    getContentPane().removeAll();
+    missileTracker.reset();
+    gameBoard.resetBoard();
+    shipPlacer.placeShips();
+    add(statusPanel, BorderLayout.NORTH);
+    add(gameBoard, BorderLayout.CENTER);
+    add(createButtonPanel(), BorderLayout.SOUTH);
+    revalidate();
+    repaint();
 }
+
+    }
+
+
+
+
+
